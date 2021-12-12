203d96e09c9922e4836a82f8f5662e78c728c714
Your Name <you@example.com>
2021-12-12 22:00:08 +0800

    add week2

---
+++ week2/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+)
+
+var notFoundCode = 4001
+var systemErrorCode = 5001
+
+func isNoRow(err error) bool {
+	return strings.HasPrefix(err.Error(), fmt.Sprintf("%d", notFoundCode))
+}
+
+func business_query(sqlString string) error {
+	result, err := Dao(sqlString)
+	if isNoRow(err) {
+		// 数据查不到的处理，可以在这里加日志
+	} else if err != nil {
+		// 正常逻辑
+	} else {
+		// 异常处理
+		return err
+	}
+	return nil
+}
+
+func mock_myqsl_query(sqlString string) (string, error) {
+	sqlString += "111"
+	return "", sql.ErrNoRows
+}
+
+func Dao(sqlString string) (string, error) {
+	result, err := mock_myqsl_query(sqlString)
+	if err == sql.ErrNoRows {
+		return "", fmt.Errorf("%d, No row Error", notFoundCode)
+	} else if err != nil {
+		return "nil", fmt.Errorf("%d, system Error", systemErrorCode)
+	} else {
+		return result, nil
+	}
+}
